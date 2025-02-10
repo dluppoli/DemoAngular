e7a7b875d5225647e7476f228ffa074f872c2ab5
Davide Luppoli <davide@davideluppoli.it>
2025-02-10 14:12:19 +0100

    Termine BookComponent

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { BooksService } from '../../services/books.service';
 import { ActivatedRoute } from '@angular/router';
+import { Book } from '../../models/Book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-book',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './book.component.html',
   styleUrl: './book.component.css'
 })
 export class BookComponent {
+  isbn:string;
+  libro:Book | undefined;
+
   constructor(private booksService: BooksService, private route: ActivatedRoute) {
-    console.log(this.route.snapshot.params['isbn']);
+    this.isbn = this.route.snapshot.params['isbn']
+    this.libro = this.booksService.getOneBook(this.isbn);
   }
 }
