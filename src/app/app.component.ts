84e1f3303d70fd3c0c1568c167d9b713ae5793e4
Davide Luppoli <davide@davideluppoli.it>
2025-02-10 10:08:41 +0100

    Direttive ngFor e ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,21 @@ import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { BooksService } from './services/books.service';
 import { Book } from './models/Book';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DemoAngular';
 
-  libro1 : Book;
+  libri : Book[];
 
   constructor(private booksService:BooksService){
-     this.libro1 = this.booksService.getBooks()[0];
+     this.libri = this.booksService.getBooks();
   }
 }
