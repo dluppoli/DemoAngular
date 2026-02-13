e34ee344567af843708c44be5b22644fa0d0c002
Davide Luppoli <davide@davideluppoli.it>
2026-02-13 10:42:01 +0100

    Products page

--- src/app/components/products-component/products-component.ts
+++ src/app/components/products-component/products-component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ShopService } from '../../services/shop-service';
+import { ProductSmall } from '../../models/ProductSmall';
+import { Products } from '../../models/Products';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-products-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products-component.html',
   styleUrl: './products-component.css',
 })
 export class ProductsComponent {
 
+  products:Products | undefined
+
+  constructor(private shopService:ShopService, private cd:ChangeDetectorRef){
+    this.shopService.getAll()
+      .subscribe(p => 
+      {
+        this.products=p
+        cd.detectChanges()
+      })
+  }
 }
